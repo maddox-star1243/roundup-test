5e87c4bc9e83304b19c6886604869fd1c2d8f926
maddox-star1243 <maddoxrminer@gmail.com>
2026-05-27 15:56:02 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,3 @@
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil((async()=>{ const keys = await caches.keys(); await Promise.all(keys.map(k=>caches.delete(k))); await clients.claim(); })()); });
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request)); });
